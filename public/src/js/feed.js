7bd928d30a5e732e149cde86df69431b5c746376
Evans Musomi <evans.musomi@gmail.com>
2018-04-21 05:37:32 +0300

    adds ability to create card on successful data fetch

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -1,8 +1,9 @@
-var shareImageButton = document.querySelector("#share-image-button");
-var createPostArea = document.querySelector("#create-post");
-var closeCreatePostModalButton = document.querySelector(
+const shareImageButton = document.querySelector("#share-image-button");
+const createPostArea = document.querySelector("#create-post");
+const closeCreatePostModalButton = document.querySelector(
   "#close-create-post-modal-btn"
 );
+const sharedMomentsArea = document.querySelector("#shared-moments");
 
 function openCreatePostModal() {
   createPostArea.style.display = "block";
@@ -30,5 +31,31 @@ function closeCreatePostModal() {
 }
 
 shareImageButton.addEventListener("click", openCreatePostModal);
-
 closeCreatePostModalButton.addEventListener("click", closeCreatePostModal);
+
+function createCard() {
+  let cardWrapper = document.createElement("div");
+  cardWrapper.className = "shared-moment-card mdl-card mdl-shadow--2dp";
+
+  let cardTitle = document.createElement("div");
+  cardTitle.className = "mdl-card__title";
+  cardTitle.style.backgroundImage = 'url("/src/images/sf-boat.jpg")';
+  cardTitle.style.backgroundSize = "cover";
+  cardTitle.style.height = "180px";
+  cardWrapper.appendChild(cardTitle);
+
+  let cardSupportingText = document.createElement("div");
+  cardSupportingText.className = "mdl-card__supporting-text";
+  cardSupportingText.textContent = "In San Francisco";
+  cardSupportingText.style.textAlign = "center";
+  cardWrapper.appendChild(cardSupportingText);
+
+  componentHandler.upgradeElement(cardWrapper);
+  sharedMomentsArea.appendChild(cardWrapper);
+}
+
+fetch("https://httpbin.org/get")
+  .then(response => response.json())
+  .then(data => {
+    createCard();
+  });

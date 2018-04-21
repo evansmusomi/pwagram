39574e66ec16848668809d643c81e3421eac0bfa
Evans Musomi <evans.musomi@gmail.com>
2018-04-21 06:00:29 +0300

    caches app shell and serves it from cache

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,28 @@
+const staticCache = "static-v1";
+const staticAssets = [
+  "/",
+  "/index.html",
+  "/src/js/app.js",
+  "/src/js/feed.js",
+  "/src/js/promise.js",
+  "/src/js/fetch.js",
+  "/src/js/material.min.js",
+  "/src/css/app.css",
+  "/src/css/feed.css",
+  "/src/images/main-image.jpg",
+  "https://fonts.googleapis.com/css?family=Roboto:400,700",
+  "https://fonts.googleapis.com/icon?family=Material+Icons",
+  "https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css"
+];
+
 self.addEventListener("install", event => {
   console.log("[SW] Installing");
+  event.waitUntil(
+    caches.open(staticCache).then(cache => {
+      console.log("[SW] Pre-caching App shell");
+      cache.addAll(staticAssets);
+    })
+  );
 });
 
 self.addEventListener("activate", event => {
@@ -8,4 +31,10 @@ self.addEventListener("activate", event => {
 
 self.addEventListener("fetch", event => {
   console.log("[SW] Fetching...", event.request.url);
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      if (response) return response;
+      return fetch(event.request);
+    })
+  );
 });

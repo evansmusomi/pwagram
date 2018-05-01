480b4cd7469843be885d593779ba48d25fde0337
Evans Musomi <evans.musomi@gmail.com>
2018-05-01 02:48:57 +0000

    reacts to notification interactions: clicks and closing

--- public/sw.js
+++ public/sw.js
@@ -164,4 +164,23 @@ self.addEventListener("sync", event => {
         })
     );
   }
-})
+});
+
+self.addEventListener("notificationclick", event => {
+  let notification = event.notification;
+  let action = event.action;
+  
+  console.log(notification);
+  
+  if (action === "confirm"){
+    console.log("Confirm was chosen");
+  }else{
+    console.log(action);
+  }
+  
+  notification.close();
+});
+
+self.addEventListener("notificationclose", event => {
+  console.log("Notification was closed", event);
+});

23e27c753396f091b18fe5df52401b16b1a7f9fd
Evans Musomi <evans.musomi@gmail.com>
2018-05-03 04:12:25 +0000

    creates push subscriptions and saves to firebase

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,6 +1,7 @@
 // Globals
 var deferredPrompt;
 const enableNotificationsButtons = document.querySelectorAll(".enable-notifications");
+let registration;
 
 // Polyfills
 if (!window.Promise) {
@@ -47,17 +48,57 @@ function displayConfirmNotification(){
   }
 }
 
+function configurePushSubscription(){
+  if (!('serviceWorker' in navigator)){
+    return;
+  }
+  
+  navigator.serviceWorker.ready.then(swRegistration => {
+    registration = swRegistration;
+    return swRegistration.pushManager.getSubscription();
+  }).then(subscription => {
+    if(subscription === null){
+      // create a new subscription
+      let vapidPublicKey = "BIu6jBHCMIujAAcizZT_KypQABl7h3D46EQUsj_pPRPsF7Ha0E4t50A7d-oMqprM5GwvQXSQMBHpWcy4MDZGrF0";
+      let applicationServerKey = urlBase64ToUint8Array(vapidPublicKey);
+      
+      return registration.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: applicationServerKey
+      });
+    }else{
+      // we have a subscription
+      
+    }
+  }).then(newSubscription => {
+    return fetch("https://pwagramapp.firebaseio.com/subscriptions.json", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(newSubscription)
+    })
+  }).then(response => {
+    if (response.ok){
+      displayConfirmNotification(); 
+    }
+  }).catch(error => {
+    console.log(error);
+  });
+}
+
 function askForNotificationPermission(){
   Notification.requestPermission(result => {
     if(result !== "granted"){
       console.log("Permission not granted");
     }else{
-      displayConfirmNotification();
+      configurePushSubscription();
     }
   });
 }
 
-if("Notification" in window){
+if("Notification" in window && "serviceWorker" in navigator){
   enableNotificationsButtons.forEach((item, index) => {
     enableNotificationsButtons[index].style.display = "inline-block";
     enableNotificationsButtons[index].addEventListener("click", askForNotificationPermission);

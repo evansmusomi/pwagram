a1a7fe032a4f25990c8d9dd5aac8b914fb3c4a7e
Evans Musomi <evans.musomi@gmail.com>
2018-05-07 04:50:01 +0000

    adds custom indexeddb handler on workbox and dynamic cache expiration options

--- public/sw-base.js
+++ public/sw-base.js
@@ -1,5 +1,6 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js");
-
+importScripts("/src/js/idb.js");
+importScripts("/src/js/utility.js");
 
 workbox.routing.registerRoute(
     new RegExp(/.*(?:firebasestorage\.googleapis)\.com.*$/),
@@ -11,7 +12,13 @@ workbox.routing.registerRoute(
 workbox.routing.registerRoute(
     new RegExp(/.*(?:googleapis|gstatic)\.com.*$/),
     workbox.strategies.staleWhileRevalidate({
-        cacheName: "google-fonts"
+        cacheName: "google-fonts",
+        plugins: [ 
+            new workbox.expiration.Plugin({ 
+                maxEntries: 3,
+                maxAgeSeconds: 30*24*60*60
+            })
+        ]
     })
 );
 
@@ -21,6 +28,22 @@ workbox.routing.registerRoute(
         cacheName: "material-css"
     })
 );
+
+workbox.routing.registerRoute("https://pwagramapp.firebaseio.com/posts.json", args => {
+    return fetch(args.event.request).then(response => {
+      let clonedResponse = response.clone();
+      clearAllData("posts")
+        .then(() => {
+          return clonedResponse.json();
+        })
+        .then(data => {
+          for (let key in data) {
+            writeData("posts", data[key]);
+          }
+        });
+      return response;
+    });
+});
 
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute([], {});
